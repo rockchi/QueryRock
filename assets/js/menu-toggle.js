a07de5246bcbb6a6c896fa8be15150aee568600d
Rock Chi <rockc@sfu.ca>
2017-01-03 14:28:30 -0800

    Changes made to navbar and contact section
    1: .contact-item sizing changed for mobile and desktop view
    2: added in sticky navbar - appear upon scroll > last 60px of viewport
    height

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $(".buttonbox").click(function(){
       $(".menu").slideToggle("slow", function(){
-
       });
+  });
+
+  $(window).scroll(function() {
+      var ViewportHeight = $(window).height();
+      var ViewportHeight = ViewportHeight - 60
+
+      if ($(this).scrollTop() >= ViewportHeight) { // if scroll is greater/equal then 100 and hasBeenTrigged is set to false.
+        console.log("You've scrolled" + ViewportHeight + "pixels.");
+        $(".menubar").addClass("sticky");
+      }
+      else {
+        $(".menubar").removeClass("sticky");
+      }
   });
 });
