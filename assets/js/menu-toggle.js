2622c88d7bd376261be462eaa5b5df0cdcc1f55e
Rock Chi <rockc@sfu.ca>
2017-01-03 16:29:18 -0800

    Dataviz Section Header Changed
    1: added navbar transition time
    2: changed section name test to dataviz, removing prevoius dataviz
    section
    3: added buffer before dataviz section to remove awkward empty space

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
-    $(".buttonbox").click(function(){
-      $(".menu").slideToggle("slow", function(){
-      });
+    //slide out toggle for menu
+  $(".buttonbox").click(function(){
+    $(".menu").slideToggle("slow", function(){
+    });
   });
 
+
+  //sticky scroll effect - menu sticks (classes added) when scroll > Viewheight - Height of navbar
   $(window).scroll(function() {
       var ViewportHeight = $(window).height();
-      var ViewportHeight = ViewportHeight - 60
+      var ViewportHeight = ViewportHeight-60;
 
-      if ($(this).scrollTop() >= ViewportHeight) { // if scroll is greater/equal then 100 and hasBeenTrigged is set to false.
+      if ($(this).scrollTop() >= ViewportHeight) {
+        //logs point of viewport height transition
         console.log("You've scrolled" + ViewportHeight + "pixels.");
         $(".menubar").addClass("sticky");
       }
@@ -16,4 +20,16 @@ $(document).ready(function() {
         $(".menubar").removeClass("sticky");
       }
   });
+
+  //navigation smooth scrolling
+  $('.menu ul li a').click(function(event) {
+      var id = $(this).attr("href");
+      var target = $(id).offset().top;
+      $('html, body').animate({
+          scrollTop: target
+      }, 500);
+
+      event.preventDefault();
+  });
+
 });
